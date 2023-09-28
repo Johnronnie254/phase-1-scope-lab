950ceb6a3843651241efbf65c5796173b29c01a7
Ochieng John Ronnie <johnronnie18@gmail.com>
2023-09-28 22:28:46 +0300

    ggg

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+
+//function that returns best customer variable declared in global space
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer(){
+     bestCustomer = 'maybe bob';
+}
+
+
+
+
+function changeLeastFavoriteCustomer(){
+      leastFavoriteCustomer = "Thomas";
+}
